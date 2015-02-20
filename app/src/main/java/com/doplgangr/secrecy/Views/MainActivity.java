00e1b54a752e723567cdd3a38393468e0eb1c67d
L-Henke <L-Henke@users.noreply.github.com>
2015-02-20 12:40:14 +0100

    Manifest and MainActivity cleanup

--- app/src/main/java/com/doplgangr/secrecy/Views/MainActivity.java
+++ app/src/main/java/com/doplgangr/secrecy/Views/MainActivity.java
@@ -20,7 +20,6 @@
 package com.doplgangr.secrecy.Views;
 
 import android.app.AlertDialog;
-import android.app.SharedElementCallback;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -28,7 +27,6 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.preference.ListPreference;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
