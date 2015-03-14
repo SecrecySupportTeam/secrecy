a585b564b01e9046ab488da4613403cc8b25edeb
Stelian Morariu <stelian.morariu@gmail.com>
2015-03-14 14:23:12 +0200

    reverted IabHelper to original state

--- libraries/payme/src/main/java/com/github/jberkel/pay/me/IabHelper.java
+++ libraries/payme/src/main/java/com/github/jberkel/pay/me/IabHelper.java
@@ -51,8 +51,31 @@ import java.util.List;
 
 import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
-import static com.github.jberkel.pay.me.IabConsts.*;
-import static com.github.jberkel.pay.me.Response.*;
+import static com.github.jberkel.pay.me.IabConsts.API_VERSION;
+import static com.github.jberkel.pay.me.IabConsts.GET_SKU_DETAILS_ITEM_LIST;
+import static com.github.jberkel.pay.me.IabConsts.INAPP_CONTINUATION_TOKEN;
+import static com.github.jberkel.pay.me.IabConsts.RESPONSE_BUY_INTENT;
+import static com.github.jberkel.pay.me.IabConsts.RESPONSE_CODE;
+import static com.github.jberkel.pay.me.IabConsts.RESPONSE_GET_SKU_DETAILS_LIST;
+import static com.github.jberkel.pay.me.IabConsts.RESPONSE_INAPP_ITEM_LIST;
+import static com.github.jberkel.pay.me.IabConsts.RESPONSE_INAPP_PURCHASE_DATA;
+import static com.github.jberkel.pay.me.IabConsts.RESPONSE_INAPP_PURCHASE_DATA_LIST;
+import static com.github.jberkel.pay.me.IabConsts.RESPONSE_INAPP_SIGNATURE;
+import static com.github.jberkel.pay.me.IabConsts.RESPONSE_INAPP_SIGNATURE_LIST;
+import static com.github.jberkel.pay.me.Response.BILLING_UNAVAILABLE;
+import static com.github.jberkel.pay.me.Response.ERROR;
+import static com.github.jberkel.pay.me.Response.IABHELPER_BAD_RESPONSE;
+import static com.github.jberkel.pay.me.Response.IABHELPER_DISPOSED;
+import static com.github.jberkel.pay.me.Response.IABHELPER_INVALID_CONSUMPTION;
+import static com.github.jberkel.pay.me.Response.IABHELPER_MISSING_TOKEN;
+import static com.github.jberkel.pay.me.Response.IABHELPER_REMOTE_EXCEPTION;
+import static com.github.jberkel.pay.me.Response.IABHELPER_SEND_INTENT_FAILED;
+import static com.github.jberkel.pay.me.Response.IABHELPER_SUBSCRIPTIONS_NOT_AVAILABLE;
+import static com.github.jberkel.pay.me.Response.IABHELPER_UNKNOWN_ERROR;
+import static com.github.jberkel.pay.me.Response.IABHELPER_UNKNOWN_PURCHASE_RESPONSE;
+import static com.github.jberkel.pay.me.Response.IABHELPER_VERIFICATION_FAILED;
+import static com.github.jberkel.pay.me.Response.OK;
+import static com.github.jberkel.pay.me.Response.getDescription;
 import static com.github.jberkel.pay.me.model.ItemType.INAPP;
 import static com.github.jberkel.pay.me.model.ItemType.SUBS;
 
@@ -89,7 +112,7 @@ import static com.github.jberkel.pay.me.model.ItemType.SUBS;
  * @author Jan Berkel
  */
 public class IabHelper {
-    static final Intent BIND_BILLING_SERVICE = new Intent("com.android.vending.billing.InAppBillingService.BIND");
+    /* package */ static final Intent BIND_BILLING_SERVICE = new Intent("com.android.vending.billing.InAppBillingService.BIND");
 
     private Context mContext;
     private IInAppBillingService mService;
