e8bb2192b8715f5ab09510ecdc23d572065048fc
L-Henke <L-Henke@users.noreply.github.com>
2015-04-07 20:44:22 +0200

    Use immersive mode in FilePhotoActivity

--- app/src/main/java/com/doplgangr/secrecy/activities/FilePhotoActivity.java
+++ app/src/main/java/com/doplgangr/secrecy/activities/FilePhotoActivity.java
@@ -2,6 +2,7 @@ package com.doplgangr.secrecy.activities;
 
 import android.app.Activity;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
@@ -94,5 +95,21 @@ public class FilePhotoActivity extends FragmentActivity {
                     null);
         }
         event.progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                mViewPager.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            }
+        }
     }
 }
